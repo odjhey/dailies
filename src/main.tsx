6803f7a5f41fa9e77a1d31b9566a590f20ed30b8
odjhey <odjhey@gmail.com>
2023-05-24 16:01:49 +0800

    chore: configure linter

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { registerSW } from 'virtual:pwa-register'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
 
 // const updateSW = registerSW({
